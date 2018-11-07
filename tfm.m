a782e72534bbb60c3fdafb7b94cb9b99cd6d670c
SarahAmpe <44272565+SarahAmpe@users.noreply.github.com>
2018-11-07 21:39:19 +0100

    tfm probeersel
    Nog een probleempje me bv arraysetup en t ?

---
+++ tfm.m
@@ -0,0 +1,13 @@
+function [intensity] = tfm(fullMat ,x,z, c, arraySetup)
+trans = lenght(arraySetup);
+intensity = 0;
+t = randomint(0,5);
+for transmitter = 1:trans
+    for receiver = 1:trans
+        time = sqrt((transmitter-x)^2+z^2) + sqrt((receiver-x)^2+z^2);
+        time = time/c;
+        intensity = intensity + fullMat(transmitter,receiver,t)*time;
+    end
+end
+intensity = abs(intensity);
+        

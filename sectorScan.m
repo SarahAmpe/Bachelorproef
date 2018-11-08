e8a71339526f4db32f5821d41be1deacc56835e4
SarahAmpe <44272565+SarahAmpe@users.noreply.github.com>
2018-11-08 14:49:14 +0100

    Poolcoordinaat voor theta

--- sectorScan.m
+++ sectorScan.m
@@ -11,7 +11,9 @@ r = sqrt( (x - arrCenter)^2 + z^2 ); % Propagation distance from array center
 intensity = 0;
 for transmit = 1:size(fullMat, 1)
     for receive = 1:size(fullMat, 2)
-        th = % Required beam steer angle with respect to the array normal
+        th = arctan(z/x) % Required beam steer angle with respect to the array normal
+        if th < 0:
+            th = th + pi
         xtx = arrSetup(transmit); % Transmitter position
         xrx = arrSetup(receive); % Receiver position
         time = 2*r + (xtx + xrx)*sin(th);

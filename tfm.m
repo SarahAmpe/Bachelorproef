7ec88eb04a602d1fa24fdb06521f9fd340a73cb2
RobbeDHondt <44818909+RobbeDHondt@users.noreply.github.com>
2018-11-08 09:11:12 +0100

    Indexen verandert naar posities + documentatie
    transmitter en receiver geven de indices in de full matrix weer, maar we willen de fysische x-locatie van het array element (te vinden in arraySetup)
    Ook documentatie toegevoegd :)

--- tfm.m
+++ tfm.m
@@ -1,13 +1,23 @@
 function [intensity] = tfm(fullMat ,x,z, c, arraySetup)
+% Calculates intensity of the Total Focusing Method at (x,z)
+% Input fullMat = full matrix of time domain signals
+%       x = position of the point of interest along the array axis
+%       z = position of the point of interest normal to the array surface
+%       c = sound speed in the medium
+%       arraySetup = vector of x coordinates of the array elements
+
 trans = length(arraySetup);
 intensity = 0;
 t = randomint(0,5);
 for transmitter = 1:trans
     for receiver = 1:trans
-        time = sqrt((transmitter-x)^2+z^2) + sqrt((receiver-x)^2+z^2);
+        xtx = arraySetup(transmitter);
+        xrx = arraySetup(receive);
+        time = sqrt((xtx-x)^2+z^2) + sqrt((xrx-x)^2+z^2);
         time = time/c;
         intensity = intensity + fullMat(transmitter,receiver,t)*time;
     end
 end
 intensity = abs(intensity);
         
+end

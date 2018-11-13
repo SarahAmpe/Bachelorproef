1a11eab0412a7be477fda1432034c505777c670a
SarahAmpe <44272565+SarahAmpe@users.noreply.github.com>
2018-11-13 09:31:38 +0100

    Change language and comment

--- wave.m
+++ wave.m
@@ -1,4 +1,6 @@
-function signaal = wave(A,f,t)
+function signal = wave(A,f,t)
+% Simulates a wave as input for fmc, by multiplying the cosine with a gaussian signal
 
-golf = A*cos(f*t);
-signaal = exp(-1/2*t.^2).*golf;
+
+cosWave = A*cos(f*t);
+signal = exp(-1/2*t.^2).*cosWave;

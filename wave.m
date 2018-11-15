61792a10c98d80a4e3554816aec60a009ef17de0
Sophie98 <44099965+Sophie98@users.noreply.github.com>
2018-11-15 15:07:38 +0100

    documentatie

--- wave.m
+++ wave.m
@@ -1,4 +1,13 @@
 function signal = wave(A,f,t)
+% This function constructs an Gaussian windowed ouput signal
+
+% INPUT:
+% A = amplitude of the signal
+% f = frequentie of the signal
+% t = time-sequence for the signal
+
+% OUTPUT:
+% signal = gaussian windowed cosinus signal.
 
 wave = A*cos(f*t);
 signal = exp(-1/2*t.^2).*wave;

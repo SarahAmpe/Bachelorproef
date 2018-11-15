9ad49aaeeaadff7994cf51f61e5f7b4cc5495b2d
Sophie98 <44099965+Sophie98@users.noreply.github.com>
2018-11-15 15:17:44 +0100

    documentatie

--- PWI.m
+++ PWI.m
@@ -1,7 +1,16 @@
 function M = PWI(H,angles,pitch,c)
+% This function simulates the Plane Wave Imaging matrix of a phase array
+% based on the Full Matrix Capture.
+
+% INPUT
+% H = frequency domain matrix from Full Matrix Capture
+% angles = vector containing the initial and final angle and the number of
+%   angles at which the waves are transmitted.
+% pitch = the pitch of the phased array.
+% c = velocity of the wave in the material.
 
 N = size(H,1); % Number of elements
-W = size(H,3);
+W = size(H,3); 
 
 startBeta = angles(1);
 endBeta = angles(2);

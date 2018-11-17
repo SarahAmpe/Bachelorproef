c28d426e4790a216cd91a8c83e6f1cf803d4c095
Sophie98 <sophie.decoppel@student.kuleuven.be>
2018-11-17 14:58:18 +0100

    testen toegevoegd

--- bachelorproef.m
+++ bachelorproef.m
@@ -1,15 +1,30 @@
-% testing van de wave functie:
+%% testing van de wave functie:
 t = 0.01:0.01:4;
 plot(t,wave(2,4*pi,t));
 
-% FMC invoerwaardes en testing
+%% FMC invoerwaardes en testing
 c = 3*10^6;
-waveInfo = [2,2*pi,0.01:0.01:4];
-materialInfo = [c,2,2];
-elementInfo = [4,1,2];
+xref = 2;
+zref = 2;
+numElements = 4;
+elementWidth = 1;
+pitch = 2;
+waveInfo = [2,2*pi,t];
+materialInfo = [c,xref,zref];
+elementInfo = [numElements,elementWidth,pitch];
 
 [fmc,S,F,d,pr,pt,A] = FMC(waveInfo,materialInfo,elementInfo);
-array = [-2, -1, 0, 1];
-I = tfm(fmc, 1, 2, c, array);
+
+%% TFM testing
+arraySetup = (0:(numElements-1)) - (numElements-1)*elementWidth/2;
+x = xref;
+z = zref;
+I = tfm(fmc, x, z, c, arraySetup);
+
+%% planeScan testing
+x = xref;
+z = zref;
+I = planeScan(fmc, x, z, D, c, arrSetup)
+
 
 

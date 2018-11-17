8882b4a7c8242ddd9cd1a7c9f89dcb5c542faa4f
Sophie98 <sophie.decoppel@student.kuleuven.be>
2018-11-17 15:40:23 +0100

    kleine invoergegevens aangepast

--- bachelorproef.m
+++ bachelorproef.m
@@ -1,14 +1,14 @@
 %% testing van de wave functie:
-t = 0.01:0.01:8;
+t = 0.01:0.005:4;
 plot(t,wave(2,4*pi,t));
 
 %% FMC invoerwaardes en testing
 c = 200;
-xref = 0;
-zref = 3;
+xref = 3;
+zref = 10;
 numElements = 20;
 elementWidth = 1;
-pitch = 2;
+pitch = 1.5;
 waveInfo = [2,2*pi,t];
 materialInfo = [c,xref,zref];
 elementInfo = [numElements,elementWidth,pitch];

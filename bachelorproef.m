8afa667280274d0eec8eaed6906c9366f6a1a3e6
Sophie98 <44099965+Sophie98@users.noreply.github.com>
2018-11-19 16:16:01 +0100

    aanpassingen tijd

--- bachelorproef.m
+++ bachelorproef.m
@@ -1,11 +1,11 @@
 %% testing van de wave functie:
-t = 0.01:0.005:4;
+t = 0.01:0.01:4;
 plot(t,wave(2,4*pi,t));
 
 %% FMC invoerwaardes en testing
 c = 200;
-xref = 0;
-zref = 5;
+xref = 3;
+zref = 10;
 numElements = 20;
 elementWidth = 1;
 pitch = 1.5;
@@ -16,27 +16,21 @@ elementInfo = [numElements,elementWidth,pitch];
 [fmc,S] = FMC(waveInfo,materialInfo,elementInfo);
 
 %% TFM testing
-I = zeros(20);
 arraySetup = (0:(numElements-1)) - (numElements-1)*elementWidth/2;
-step_x = 0;
-step_z = 0;
-for m = -10:10
-    step_x = step_x +1;
-    step_z = 0;
-    for n = 0:0.5:10
-        step_z = step_z + 1;
-        x = m;
-        z = n;
-        I(step_x, step_z) = tfm(fmc,t, x, z, c, arraySetup);
+for m = 1:numElements
+    for n = 1:numElements
+        x = (m-1)- (numElements-1)*elementWidth/2;
+        z = (n-1);
+        I(n,m) = tfm(fmc,t, x, z, c, arraySetup);
     end
 end
-imagesc(-10:10,0:0.5:10,I)
+imagesc((0:(numElements-1)) - (numElements-1)*elementWidth/2,0:(numElements-1),I)
 colorbar
-% %% planeScan testing
-% x = xref;
-% z = zref;
-% 
-% I = planeScan(fmc, x, z,D , c, arrSetup)
+%% planeScan testing
+x = xref;
+z = zref;
+
+I = planeScan(fmc, x, z,D , c, arrSetup)
 
 
 

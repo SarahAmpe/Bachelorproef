1d1587b5495097d43125ee620adb2360fe3b7c35
RobbeDHondt <44818909+RobbeDHondt@users.noreply.github.com>
2018-11-20 10:22:18 +0100

    Andere inputwaarden

--- bachelorproef.m
+++ bachelorproef.m
@@ -1,52 +1,29 @@
-%% testing van de wave functie:
-t = 0.01:0.01:8; % niet aanpassen! tfm is hiervan afhankelijk
+%% FMC input (+ test wavefunctie)
+t = linspace(1e-6, 1e-5, 1000); % niet aanpassen! tfm is hiervan afhankelijk
 plot(t,wave(2,4*pi,t));
-
-%% FMC invoerwaardes en testing
-c = 200;
-xref = -0.09;
-zref = 0.57;
-numElements = 20;
-elementWidth = 0.05;
-pitch = 0.06;
-waveInfo = [2,2*pi,t];
+c = 3e6;
+xref = 5;
+zref = 5;
+numElements = 64;
+elementWidth = 0.53;
+pitch = 0.63;
+waveInfo = [1, 5e6,t];
 materialInfo = [c,xref,zref];
 elementInfo = [numElements,elementWidth,pitch];
 
 [fmc,S] = FMC(waveInfo,materialInfo,elementInfo);
-
-%% TFM testing
-I = zeros(20);
-arraySetup = (-(numElements-1)*pitch/2:pitch:(numElements-1)*pitch/2);
-aantalx= 20;% nauwkeurigheid (aantal punten dat je wilt plotten)
-aantalz= 20;
-xmin = -(numElements-1)*pitch/2;
-xmax = (numElements-1)*pitch/2;
-zmin = 0.05;
-zmax = 1;
-stepx = (numElements-1)*pitch/aantalx;
-stepz = (zmax-zmin)/aantalx;
-for m = 1:aantalx+1
-    for n = 1:aantalz+1
-        x = xmin + (m-1)*stepx;
-        z = zmin + (n-1)*stepz;
-        I(n,m) = tfm(fmc,t, x, z, c, arraySetup);
-    end
-end
-imagesc(xmin:stepx:xmax,zmin:stepz:zmax,I)
-colorbar
 
 %% planeScan testing
 D = 5*pitch;
 
 I = zeros(20);
 arraySetup = (-(numElements-1)*pitch/2:pitch:(numElements-1)*pitch/2);
-aantalx= 20;% nauwkeurigheid (aantal punten dat je wilt plotten)
-aantalz= 20;
+aantalx = 100;% nauwkeurigheid (aantal punten dat je wilt plotten)
+aantalz = 100;
 xmin = -(numElements-1)*pitch/2;
 xmax = (numElements-1)*pitch/2;
-zmin = 0.05;
-zmax = 1;
+zmin = 0.01;
+zmax = 10;
 stepx = (numElements-1)*pitch/aantalx;
 stepz = (zmax-zmin)/aantalx;
 for m = 1:aantalx+1
@@ -56,10 +33,10 @@ for m = 1:aantalx+1
         I(n,m) = planeScan(fmc, t, x, z, D, c, arraySetup);
     end
 end
-imagesc(xmin:stepx:xmax,zmin:stepz:zmax,I)
+imagesc(xmin:stepx:xmax, zmin:stepz:zmax, I)
 colorbar
 
-%% secotorScan testing
+%% sectorScan testing
 
 I = zeros(20);
 arraySetup = (-(numElements-1)*pitch/2:pitch:(numElements-1)*pitch/2);
@@ -103,3 +80,25 @@ for m = 1:aantalx+1
 end
 imagesc(xmin:stepx:xmax,zmin:stepz:zmax,I)
 colorbar
+
+%% TFM testing
+I = zeros(20);
+arraySetup = (-(numElements-1)*pitch/2:pitch:(numElements-1)*pitch/2);
+aantalx= 20;% nauwkeurigheid (aantal punten dat je wilt plotten)
+aantalz= 20;
+xmin = -(numElements-1)*pitch/2;
+xmax = (numElements-1)*pitch/2;
+zmin = 0.05;
+zmax = 1;
+stepx = (numElements-1)*pitch/aantalx;
+stepz = (zmax-zmin)/aantalx;
+for m = 1:aantalx+1
+    for n = 1:aantalz+1
+        x = xmin + (m-1)*stepx;
+        z = zmin + (n-1)*stepz;
+        I(n,m) = tfm(fmc,t, x, z, c, arraySetup);
+    end
+end
+imagesc(xmin:stepx:xmax,zmin:stepz:zmax,I)
+colorbar
+

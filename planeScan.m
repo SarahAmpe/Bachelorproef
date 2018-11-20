bb4782c7c98ee200e3146135c8de222ddf11a5c8
RobbeDHondt <44818909+RobbeDHondt@users.noreply.github.com>
2018-11-20 10:36:02 +0100

    Fixing documentation
    Documentatie conform maken aan andere documenten en t een duidelijkere naam gegeven (ik moest altijd te veel nadenken)

--- planeScan.m
+++ planeScan.m
@@ -1,28 +1,24 @@
-function [intensity] = planeScan(fullMat, t, x, z, D, c, arrSetup)
-% Calculates intensity of the plane B-scan image at (x,z)
-% Input fullMat = full matrix of time domain signals
-%       t = time sequence of fullMat
-%       x = position of the point of interest along the array axis
-%       z = position of the point of interest normal to the array surface
-%       D = aperture width
-%       c = sound speed in the medium
-%       arrSetup = vector of x coordinates of the array elements
+function [intensity] = planeScan(fullMat, sampleTimes, x, z, D, c, arrSetup)
+% PLANESCAN Calculates intensity of the plane B-scan image at (x,z)
+%
+% INPUT: 
+% fullMat     = full matrix of time domain signals
+% sampleTimes = time sequence of fullMat
+% x           = position of the point of interest along the array axis
+% z           = position of the point of interest normal to the array surface
+% D           = aperture width
+% c           = sound speed in the medium
+% arrSetup    = vector of x coordinates of the array elements
 
 arrElems = abs(arrSetup - x) <= D/2; % Array elements that matter
 time = 2*z/c; % Appropriate time
-%lowerTime = floor(time*100)/100;
-%upperTime = ceil(time*100)/100;
-%if lowerTime ==0
-%    lowerTime = t(1);
-%elseif upperTime >= t(end)
-%    upperTime = t(end);
-%end
-if any(t <= time)
-    lowerTime = find(t <= time, 1, 'last');
+
+if any(sampleTimes <= time)
+    lowerTime = find(sampleTimes <= time, 1, 'last'); % Largest lower bound in time sequence of fullMat
 else
     lowerTime = 1;
 end
-upperTime = min(lowerTime+1, length(t));
+upperTime = min(lowerTime+1, length(sampleTimes)); % Smallest upper bound in the time sequence of fullMat
 
 lowerSignal = fullMat(arrElems, arrElems, lowerTime);
 upperSignal = fullMat(arrElems, arrElems, upperTime);

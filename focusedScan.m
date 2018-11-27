0590b111b1b0e3e27bd4e915fa678cda952633bf
RobbeDHondt <44818909+RobbeDHondt@users.noreply.github.com>
2018-11-27 22:47:11 +0100

    Veralgemening naar volledige grid
    Deze versie werkt, maar geeft niet de gewenste output. Kan ook nog heel wat efficiënter door te vectoriseren, maar is moeilijker omdat nu ook x-afhankelijke posities

--- focusedScan.m
+++ focusedScan.m
@@ -1,27 +1,34 @@
-function [intensity] = focusedScan(fullMat,t, x, z, D, c, arrSetup)
-% Calculates intensity of the plane B-scan image at (x,z)
-% Input fullMat = full matrix of time domain signals
-%       t = time sequence of fullMat
-%       x = position of the point of interest along the array axis
-%       z = position of the point of interest normal to the array surface
-%       D = aperture width
-%       c = sound speed in the medium
-%       arrSetup = vector of x coordinates of the array elements
+function [intensity] = focusedScan(fmc, t, x, z, D, c, arrSetup)
+% FOCUSEDSCAN Calculates intensity of the plane B-scan image for each point in a grid
+% INPUT: 
+    % fmc      = full matrix of time domain signals
+    % t        = time sequence of fullMat
+    % x        = array with positions of the points of interest along the array axis
+    % z        = array with positions of the points of interest normal to the array surface
+    % D        = aperture width
+    % c        = sound speed in the medium
+    % arrSetup = vector of x coordinates of the array elements
+% OUTPUT:
+    % intensity = matrix with intensity for each (x,z) position
 
-arrElems = abs(arrSetup - x) <= D/2; % Array elements that matter
-subMat = fullMat(arrElems, arrElems, :); % Select the relevant submatrix
-arrSetup = arrSetup(arrElems); % Overwrite arrSetup with relevant transducers
-intensity = 0;
-for transmit = 1:size(subMat, 1)
-    for receive = 1:size(subMat, 2)
-        xtx = arrSetup(transmit); % Transmitter position
-        xrx = arrSetup(receive); % Receiver position
-        time = ( sqrt((xtx-x)^2 + z^2) + sqrt((xrx-x)^2 + z^2) )/c;
-        [lowerTime,upperTime] = time2(t,time);
-        lowerSignal = subMat(transmit, receive, lowerTime);
-        upperSignal = subMat(transmit, receive, upperTime);
-        signals = (lowerSignal + upperSignal)/2; 
-        intensity = intensity + signals;
+intensity = zeros(length(z), length(x));
+for m = 1:length(x)
+    arrElems = abs(arrSetup - x(m)) <= D/2; % Array elements that matter
+    subMat = fmc(arrElems, arrElems, :); % Select the relevant submatrix
+    arrSetup = arrSetup(arrElems); % Overwrite arrSetup with relevant transducers
+    for n = 1:length(z)
+        for transmit = 1:size(subMat, 1)
+            for receive = 1:size(subMat, 2)
+                xtx = arrSetup(transmit); % Transmitter position
+                xrx = arrSetup(receive); % Receiver position
+                time = ( sqrt((xtx-x(m))^2 + z(n)^2) + sqrt((xrx-x(m))^2 + z(n)^2) )/c;
+                [lowerTime,upperTime] = time2(t,time);
+                lowerSignal = envelope(subMat(transmit, receive, lowerTime));
+                upperSignal = envelope(subMat(transmit, receive, upperTime));
+                signals = (lowerSignal + upperSignal)/2; 
+                intensity(n,m) = intensity(n,m) + signals;
+            end
+        end
     end
 end
 

3b3e369fc3fe1e739b02b16533c409e40a42c2e1
RobbeDHondt <44818909+RobbeDHondt@users.noreply.github.com>
2018-11-28 09:55:29 +0100

    Werkend maar niet super efficiënt
    Array met 32 elementen en een grid van 32x32 voor een eenmalige test:
    12.75 seconden voor originele functie (foute plot)
    18.56 seconden voor nieuwe functie (juiste plot)

--- tfm.m
+++ tfm.m
@@ -10,19 +10,39 @@ function [intensity] = tfm(fullMat,t,x,z, c, arraySetup)
 % OUTPUT:
     % intensity = value of the intensity of the TFM image at (x,z)
 
-I = zeros(length(z),length(x));
-for n = 1:length(z)
-    for m = 1:length(x)
-        xtx = arraySetup;
-        xrx = xtx';
-        time = sqrt((xtx-x(m)).^2+z(n)^2) + sqrt((xrx-x(m)).^2+z(n)^2);
-        time = time/c;
-        signal = permute(fullMat,[3,1,2]);
-        signal = envelope(signal(:,:));
-        S = interp1(t,signal,time);
-        S = reshape(S,length(arraySetup),[]);
-        A = S(:,1:length(arraySetup)+1:end);
-        I(n,m) = sum(sum(A));
+% I = zeros(length(z),length(x));
+% for n = 1:length(z)
+%     for m = 1:length(x)
+%         xtx = arraySetup;
+%         xrx = xtx';
+%         time = sqrt((xtx-x(m)).^2+z(n)^2) + sqrt((xrx-x(m)).^2+z(n)^2);
+%         time = time/c;
+%         signal = permute(fullMat,[3,1,2]);
+%         signal = envelope(signal(:,:));
+%         S = interp1(t,signal,time);
+%         S = reshape(S,length(arraySetup),[]);
+%         A = S(:,1:length(arraySetup)+1:end);
+%         I(n,m) = sum(sum(A));
+%     end
+% end
+% intensity = I;
+
+intensity = zeros(length(z), length(x));
+for m = 1:length(x)
+    for n = 1:length(z)
+        for transmit = 1:size(fullMat, 1)
+            for receive = 1:size(fullMat, 2)
+                xtx = arraySetup(transmit); % Transmitter position
+                xrx = arraySetup(receive); % Receiver position
+                time = ( sqrt((xtx-x(m))^2 + z(n)^2) + sqrt((xrx-x(m))^2 + z(n)^2) )/c;
+                [lowerTime,upperTime] = time2(t,time);
+                signal = permute(fullMat(transmit, receive, :), [3 1 2]);
+                signal = envelope(signal);
+                lowerSignal = signal(lowerTime);
+                upperSignal = signal(upperTime);
+                signal = (lowerSignal + upperSignal)/2;
+                intensity(n,m) = intensity(n,m) + signal;
+            end
+        end
     end
 end
-intensity = I;

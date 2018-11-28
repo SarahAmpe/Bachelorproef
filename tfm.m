b357804f7e0d7a77360bd0b8a2f19be8710092c6
Sophie98 <sophie.decoppel@student.kuleuven.be>
2018-11-28 01:07:22 +0100

    vectorizatie van tfm

--- tfm.m
+++ tfm.m
@@ -2,23 +2,24 @@ function [intensity] = tfm(fullMat,t,x,z, c, arraySetup)
 % Calculates intensity of the Total Focusing Method at (x,z)
 % Input fullMat = full matrix of time domain signals
 %       t = time sequence of fullMat
-%       x = position of the point of interest along the array axis
-%       z = position of the point of interest normal to the array surface
+%       x = positions of the point of interest along the array axis
+%       z = positions of the point of interest normal to the array surface
 %       c = sound speed in the medium
 %       arraySetup = vector of x coordinates of the array elements
 
-trans = length(arraySetup);
-intensity = 0;
-for transmitter = 1:trans
-    for receiver = 1:trans
-        xtx = arraySetup(transmitter);
-        xrx = arraySetup(receiver);
-        time = sqrt((xtx-x)^2+z^2) + sqrt((xrx-x)^2+z^2);
+I = zeros(length(z),length(x));
+for n = 1:length(z)
+    for m = 1:length(x)
+        xtx = arraySetup;
+        xrx = xtx';
+        time = sqrt((xtx-x(m)).^2+z(n)^2) + sqrt((xrx-x(m)).^2+z(n)^2);
         time = time/c;
-        [lowerTime,upperTime] = time2(t,time);
-        lowerSignal = fullMat(transmitter, receiver, lowerTime);
-        upperSignal = fullMat(transmitter, receiver, upperTime);
-        signals = (lowerSignal + upperSignal)/2; % Linearly interpolating time
-        intensity = intensity + signals;
+        signal = permute(fullMat,[3,1,2]);
+        signal = envelope(signal(:,:));
+        S = interp1(t,signal,time);
+        S = reshape(S,length(arraySetup),[]);
+        A = S(:,1:length(arraySetup)+1:end);
+        I(n,m) = sum(sum(A));
     end
 end
+intensity = I;

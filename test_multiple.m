e3b4323f3264c886bf004e5cebd43e9c7f645a00
Sophie98 <sophie.decoppel@student.kuleuven.be>
2018-12-03 20:06:36 +0100

    kleine aanpassing voor mappen

--- test_multiple.m
+++ test_multiple.m
@@ -1,8 +1,8 @@
 clear;
 close all;
-% addpath('0. Hulpfuncties')
-% addpath('1. Originele FMC en PWI')
-% addpath('2. Project lijm')
+addpath('Hulpfuncties')
+addpath('OrigineleFMCenPWI')
+addpath('MultipleLayers')
 
 %% FMC multiple input (+ test wavefunctie)
 t = linspace(-1e-5, 1e-5, 2048); 
@@ -53,13 +53,21 @@ c_a = 1.5e6; % Sound velocity in water
 c_b = 6.3e6; % Longitudinaal in aluminium
 c_c = 3.1e6; % Transversaal in aluminium
 c = [c_a c_b c_b c_c];
+xref = -3;
+zref = 5.5;
+z_in = 5;
+numElements = 32;
+elementWidth = 0.53;
+pitch = 0.63;
+waveInfo = [1, 5e6,t];
 materialInfo = [xref,zref, z_in,c];
-[fmc,~] = FMC_multiple(waveInfo,materialInfo,elementInfo);
+elementInfo = [numElements,elementWidth,pitch];
+[~,S] = FMC_multiple(waveInfo,materialInfo,elementInfo);
 
-angles = linspace(-60,60,120);
+angles = linspace(-pi/20,pi/20,120);
 pwi = PWI(t,S,angles,pitch,c);
 
-I = PWI_image(pwi,t, x, z, z_in, c_a, c_b, arraySetup,angles);
+I = PWI_image(pwi,t, x, z, z_in, [c_a, c_b], arraySetup,angles);
 imagesc(x,z,I)
 colorbar
 hold on

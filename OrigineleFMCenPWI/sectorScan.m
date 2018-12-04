5a4a4a3913a40c4e73ca8037bca9748158eb8027
RobbeDHondt <44818909+RobbeDHondt@users.noreply.github.com>
2018-12-04 16:22:32 +0100

    Werkende versie sectorscan

--- OrigineleFMCenPWI/sectorScan.m
+++ OrigineleFMCenPWI/sectorScan.m
@@ -10,35 +10,35 @@ function [intensity] = sectorScan(fmc, t, x, z, c, arrSetup)
 % OUTPUT:
     % intensity = matrix with intensity for each (x,z) position
 
-% arrCenter = median(arrSetup);
 % fmc = permute(fmc, [3 1 2]); % LET OP FMC IS PERMUTED
 % intensity = zeros(length(z), length(x));
-% r = sqrt( (x - arrCenter).^2 + z'.^2 ); % Matrix with propagation distances from array center
-% th = atan(z'./(x - arrCenter)); % Matrix with beam steer angles (with respect to array normal)
-% for m = 1:length(x)
-%     for n = 1:length(z)
-%         time = 2*r(m,n) + (arrSetup + arrSetup')*sin(th(m,n));
-%         time = time/c;
-%         for transmit = 1:length(arrSetup)
-%             for receive = 1:length(arrSetup)
-%                 signal = envelope(fmc(:,transmit,receive)); % Let op, fmc is permuted
-%                 signal = interp1(t,signal,time(transmit,receive));
-%                 intensity(n,m) = intensity(n,m) + signal;
-%             end
-%         end
+% r = sqrt((x).^2 + z'.^2); % Matrix with propagation distances from array center
+% st = x./r;
+% for transmit = 1:length(arrSetup)
+%     for receive = 1:length(arrSetup)
+%         time = (2*r + (arrSetup(transmit) + arrSetup(receive)).*(x./r))/c;
+%         signal = envelope(fmc(:,transmit,receive)); % Let op, fmc is permuted
+%         I = interp1(t,signal,time);
+%         intensity = intensity + I;
 %     end
 % end
 
 fmc = permute(fmc, [3 1 2]); % LET OP FMC IS PERMUTED
 intensity = zeros(length(z), length(x));
 r = sqrt((x).^2 + z'.^2); % Matrix with propagation distances from array center
+st = x./r;
 for transmit = 1:length(arrSetup)
     for receive = 1:length(arrSetup)
-        time = (2*r + (arrSetup(transmit) + arrSetup(receive)).*(x./r))/c;
+        xtx = arrSetup(transmit);
+        xrx = arrSetup(receive);
+        time =        sqrt(r.^2 + xtx^2 - sign(xtx)*2*xtx*r.*st.*sign(x));
+        time = time + sqrt(r.^2 + xrx^2 - sign(xrx)*2*xrx*r.*st.*sign(x));
+        time = time/c;
         signal = envelope(fmc(:,transmit,receive)); % Let op, fmc is permuted
         I = interp1(t,signal,time);
         intensity = intensity + I;
     end
 end
+
 
 end

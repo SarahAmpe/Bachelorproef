09623799b0e41776640257c97020e4d3c30b11b3
RobbeDHondt <44818909+RobbeDHondt@users.noreply.github.com>
2018-12-04 13:37:20 +0100

    nieuwste versie

--- MultipleLayers/tfm_multiple.m
+++ MultipleLayers/tfm_multiple.m
@@ -1,41 +1,19 @@
-function [intensity] = tfm_multiple(fullMat,t,xref,zref,z_in, c_a, c_b, arraySetup)
-% TFM_MULTIPLE Calculates intensity of the Total Focusing Method at (x,z) for a multiple-layered material
+function [intensity] = tfm_multiple(fullMat,t,gridx,gridz,z_in, c, arraySetup)
+% TFM_MULTIPLE Calculates intensity of the Total Focusing Method at all (x,z) pairs for a multiple-layered material
 % INPUT:
     % fullMat    = full matrix of time domain signals
     % t          = time sequence of fullMat
-    % x          = position of the point of interest along the array axis
-    % z          = position of the point of interest normal to the array surface
-    % c          = sound speed in the medium
+    % gridx      = positions of the points of interest along the array axis
+    % gridz      = positions of the points of interest normal to the array surface
+    % c          = sound speed in the media
     % arraySetup = vector of x coordinates of the array elements
 % OUTPUT:
-    % intensity = value of the intensity of the TFM image at (x,z)
-
-% trans = length(arraySetup);
-% intensity = 0;
-% for transmitter = 1:trans
-%     for receiver = 1:trans
-%         xt = arraySetup(transmitter);
-%         xr = arraySetup(receiver);
-%         
-%         func = @(x) c_b/c_a*((x-xt)*((x-xt)^2 + z_in^2)^(-1/2)) - (xref - x)*((xref -x)^2 + (zref-z_in)^2)^(-1/2);
-%         x_in = fzero(func, (xt + xref)/2); %Position where ingoing wave transits into the other material 
-% 
-%         func = @(x) c_a/c_b*((x-xref)*((x-xref)^2 + (zref-z_in)^2)^(-1/2)) - (xr-x)*((xr-x)^2 + z_in^2)^(-1/2);
-%         x_out = fzero(func, (xr + xref)/2); %Position where outgoing wave transits into the other material 
-%         
-%         timeIn = sqrt((xt-x_in)^2 + z_in^2)/c_a + sqrt((xref - x_in)^2 + (z_in - zref)^2)/c_b;
-%         timeOut = sqrt((xr-x_out)^2 + z_in^2)/c_a + sqrt((xref - x_out)^2 + (z_in - zref)^2)/c_b;
-%         time = timeIn + timeOut;
-%         
-%         signal = permute(fullMat(transmitter, receiver, :), [3 1 2]);
-%         signal = envelope(signal(:,:));
-%         I = interp1(t,signal,time); % Linearly interpolating time
-%         intensity = intensity + I;
-%     end
-% end
-
+    % intensity = values of the intensity of the TFM image
+    
+c_a = c(1);
+c_b = c(2);
 trans = length(arraySetup);
-intensity = zeros(length(zref),length(xref));
+intensity = zeros(length(gridz),length(gridx));
 x_in = intensity;
 x_out = x_in;
 
@@ -47,14 +25,14 @@ for transmitter = 1:trans
         xr = arraySetup(receiver);
         func_out = @(x,x_p,z_p) c_a/c_b*((x-x_p)*((x-x_p)^2 + (z_p-z_in)^2)^(-1/2)) - (xr-x)*((xr-x)^2 + z_in^2)^(-1/2);
         
-        for m = 1:length(xref)
-            for n = 1:length(zref)
-                x_in(n,m) = fzero(@(x) func_in(x,xref(m),zref(n)), (xt + xref(m))/2); 
-                x_out(n,m) = fzero(@(x) func_out(x,xref(m),zref(n)), (xr + xref(m))/2); 
+        for m = 1:length(gridx)
+            for n = 1:length(gridz)
+                x_in(n,m) = fzero(@(x) func_in(x,gridx(m),gridz(n)), (xt + gridx(m))/2); 
+                x_out(n,m) = fzero(@(x) func_out(x,gridx(m),gridz(n)), (xr + gridx(m))/2); 
             end
         end
-        timeIn = sqrt((xt-x_in).^2 + z_in^2)/c_a + sqrt((xref - x_in).^2 + (z_in - zref').^2)/c_b;
-        timeOut = sqrt((xr-x_out).^2 + z_in^2)/c_a + sqrt((xref - x_out).^2 + (z_in - zref').^2)/c_b;
+        timeIn = sqrt((xt-x_in).^2 + z_in^2)/c_a + sqrt((gridx - x_in).^2 + (z_in - gridz').^2)/c_b;
+        timeOut = sqrt((xr-x_out).^2 + z_in^2)/c_a + sqrt((gridx - x_out).^2 + (z_in - gridz').^2)/c_b;
         time = timeIn + timeOut;
         
         signal = permute(fullMat(transmitter, receiver, :), [3 1 2]);

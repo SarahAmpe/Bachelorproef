2a99b3b050184ea600407172c80d53e27104d5e4
RobbeDHondt <44818909+RobbeDHondt@users.noreply.github.com>
2018-12-08 11:27:08 +0100

    Hulpfunctie voor plotten

---
+++ Hulpfuncties/intensityPlot.m
@@ -0,0 +1,23 @@
+function intensityPlot(x,z,xx,zz,intensityMat,plotTitle,fileName)
+%INTENSITYPLOT Makes a detailed intensity plot of the 4 scanning methods
+%INPUT
+    % x         = Grid x-coordinates
+    % z         = Grid z-coordinates
+    % xx        = Defect x-coordinates
+    % zz        = Defect z-coordinates
+    % I         = matrix with intensity at each point
+    % plotTitle = title to be displayed above figure
+    % fileName  = what name to give the plot
+
+figure
+imagesc(x,z,intensityMat/max(max(intensityMat)))
+title(plotTitle)
+hold on
+plot(xx,zz,'kx') % Defect locations
+xlabel('x-coordinate in mm')
+ylabel('z-coordinate in mm')
+cb = colorbar;
+cb.Label.String = 'Intensity of the wave in the receiving transducers';
+saveas(gcf, fileName)
+
+end

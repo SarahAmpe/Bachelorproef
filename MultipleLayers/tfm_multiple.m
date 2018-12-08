e3f9c826a467133a0afca89f2dde234544f5d513
Sophie98 <sophie.decoppel@student.kuleuven.be>
2018-12-08 17:08:06 +0100

    foutje verbeterd
    fout voor als z-waarde kleiner was dan z_in verbeterd

--- MultipleLayers/tfm_multiple.m
+++ MultipleLayers/tfm_multiple.m
@@ -20,21 +20,24 @@ x_out = x_in;
 for transmitter = 1:trans
     xt = arraySetup(transmitter);
     func_in = @(x,x_p,z_p) c_b/c_a*((x-xt)*((x-xt)^2 + z_in^2)^(-1/2)) - (x_p - x)*((x_p -x)^2 + (z_p-z_in)^2)^(-1/2);
-    
+ 
     for receiver = 1:trans
         xr = arraySetup(receiver);
         func_out = @(x,x_p,z_p) c_a/c_b*((x-x_p)*((x-x_p)^2 + (z_p-z_in)^2)^(-1/2)) - (xr-x)*((xr-x)^2 + z_in^2)^(-1/2);
-        
-        for m = 1:length(gridx)
-            for n = 1:length(gridz)
-                x_in(n,m) = fzero(@(x) func_in(x,gridx(m),gridz(n)), (xt + gridx(m))/2); 
-                x_out(n,m) = fzero(@(x) func_out(x,gridx(m),gridz(n)), (xr + gridx(m))/2); 
+        for n = 1:length(gridz)
+            if gridz(n) > z_in
+                for m = 1:length(gridx)
+                    x_in(n,m) = fzero(@(x) func_in(x,gridx(m),gridz(n)), (xt + gridx(m))/2); 
+                    x_out(n,m) = fzero(@(x) func_out(x,gridx(m),gridz(n)), (xr + gridx(m))/2); 
+                end     
+
+                timeIn(n,:) = sqrt((xt-x_in(n,:)).^2 + z_in^2)/c_a + sqrt((gridx - x_in(n,:)).^2 + (z_in - gridz(n)).^2)/c_b;
+                timeOut(n,:) = sqrt((xr-x_out(n,:)).^2 + z_in^2)/c_a + sqrt((gridx - x_out(n,:)).^2 + (z_in - gridz(n)).^2)/c_b;
+                time(n,:) = timeIn(n,:) + timeOut(n,:);
+            else
+                time(n,:) = ( sqrt((xt-gridx).^2 + gridz(n).^2) + sqrt((xr-gridx).^2 + gridz(n).^2) )/c_a;
             end
         end
-        timeIn = sqrt((xt-x_in).^2 + z_in^2)/c_a + sqrt((gridx - x_in).^2 + (z_in - gridz').^2)/c_b;
-        timeOut = sqrt((xr-x_out).^2 + z_in^2)/c_a + sqrt((gridx - x_out).^2 + (z_in - gridz').^2)/c_b;
-        time = timeIn + timeOut;
-        
         signal = permute(fullMat(transmitter, receiver, :), [3 1 2]);
         signal = envelope(signal(:,:));
         I = interp1(t,signal,time); % Linearly interpolating time

f66af7218d40640b3729b6d41ad25ca5d8a8d439
Sophie98 <sophie.decoppel@student.kuleuven.be>
2018-12-14 14:56:05 +0100

    planeScan_multiple

---
+++ planeScan_multiple.m
@@ -0,0 +1,28 @@
+function [intensity] = planeScan_multiple(fmc, t, x, z, z_in, D, c, arrSetup)
+% PLANESCAN Calculates intensity of the plane B-scan image for each point in a grid
+% INPUT: 
+    % fmc      = full matrix of time domain signals
+    % t        = time sequence of fullMat
+    % x        = array with positions of the points of interest along the array axis
+    % z        = array with positions of the points of interest normal to the array surface
+    % D        = aperture width
+    % c        = sound speed in the medium
+    % arrSetup = vector of x coordinates of the array elements
+% OUTPUT:
+    % intensity = matrix with intensity for each (x,z) position
+
+c_a = c(1);
+c_b = c(2);
+% Building the intensity matrix columnwise
+z_far = z > z_in;
+time(z_far) = 2*z_in/c_a + 2*(z(z_far)-z_in)/c_b;
+time(not(z_far)) = 2*z(not(z_far))/c_a;
+intensity = zeros(length(z), length(x));
+for m = 1:length(x)
+    arrElems = abs(arrSetup - x(m)) <= D/2; % Array elements that matter
+    signal = permute(fmc(arrElems,arrElems,:),[3,1,2]);
+    signal = sum(envelope(signal(:,:)),2); % Take the Hilbert transform and sum for each transmitter (?)
+    intensity(:,m) = interp1(t,signal,time);
+end
+
+end
--- test_multiple.m
+++ test_multiple.m
@@ -5,19 +5,19 @@ addpath('OrigineleFMCenPWI')
 addpath('MultipleLayers')
 
 %% FMC multiple input (+ test wavefunctie)
-t = linspace(-1e-5, 1e-5, 2^15);
+t = linspace(-1e-5, 1e-5, 2^14);
 % plot(t,wave(1,5e6,t));
 c_a = 6.3e6; % Longitudinaal in aluminium
 c_b = 1.5e6; % Sound velocity in water
 c = [c_a c_b c_a];
 z_in = [5,6];
-numElements = 16;
+numElements = 32;
 elementWidth = 0.53;
 pitch = 0.63;
 waveInfo = [1, 5e6,t];
 elementInfo = [numElements,elementWidth,pitch];
 
-xx = linspace(-4,4,5); % Voor 16 transducers:
+xx = linspace(-3,3,3); % Voor 16 transducers:
     % 1 mm: linspace(-3,3,6);
     % 2 mm: linspace(-4,4,5);
     % 3 mm: linspace(-3,3,3);
@@ -29,8 +29,8 @@ end
 %% TFM testing (multiple layers)
 % testparameters:
 arraySetup = (-(numElements-1)*pitch/2:pitch:(numElements-1)*pitch/2);
-aantalx = 64; % Nauwkeurigheid (aantal punten dat je wilt plotten)
-aantalz = 64;
+aantalx = 100; % Nauwkeurigheid (aantal punten dat je wilt plotten)
+aantalz = 100;
 zmin = 5;
 zmax = 6;
 xmin = -(numElements-1)*pitch/2;
@@ -48,7 +48,33 @@ plot([xmin,xmax],[z_in(1),z_in(1)],'r','LineWidth',2)
 plot([xmin,xmax],[z_in(2),z_in(2)],'r','LineWidth',2)
 hold off
 
+%% PlaneScan testing (multiple layers)
+% testparameters:
+D = 5*pitch;
+arraySetup = (-(numElements-1)*pitch/2:pitch:(numElements-1)*pitch/2);
+aantalx = 500; % Nauwkeurigheid (aantal punten dat je wilt plotten)
+aantalz = 500;
+zmin = 5.25;
+zmax = 5.9;
+xmin = -(numElements-1)*pitch/2;
+xmax = (numElements-1)*pitch/2;
+z = linspace(zmin,zmax,aantalz);
+x = linspace(xmin,xmax,aantalx);
 
+% figuur
+figure
+I = planeScan_multiple(fmc,t, x, z, z_in(1), D, [c_a,c_b], arraySetup);
+imagesc(x,z,I)
+plotTitle = ['Plane Scan multiple (', num2str(xx), ',' , num2str(5.5), ')' ];
+title(plotTitle)
+xlabel('x-coordinate in mm')
+ylabel('z-coordinate in mm')
+cb = colorbar;
+cb.Label.String = 'Intensity of the wave in the receiving transducers';
+hold on
+plot([xmin,xmax],[z_in(1),z_in(1)],'r','LineWidth',2)
+plot([xmin,xmax],[z_in(2),z_in(2)],'r','LineWidth',2)
+hold off
 %% PWI testing (multiple layers)
 % testparameters:
 t = linspace(-1e-4, 1e-4, 2^15);
